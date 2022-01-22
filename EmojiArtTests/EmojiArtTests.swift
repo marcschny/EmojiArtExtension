7182c254cf81c41426085248ead3ec397055a762
Marc Schnydrig <marcschny@MacBook-Pro-von-Marc.local>
2022-01-22 20:31:35 +0100

    some unit tests

--- EmojiArtTests/EmojiArtTests.swift
+++ EmojiArtTests/EmojiArtTests.swift
@@ -1,21 +1,58 @@
-//
-//  EmojiArtTests.swift
-//  EmojiArtTests
-//
-//  Created by Oliver Gepp on 28.10.21.
-//
-
 import XCTest
 @testable import EmojiArt
 
 class EmojiArtTests: XCTestCase {
+    
+    private var document: EmojiArtDocumentViewModel!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        document = EmojiArtDocumentViewModel.init()
     }
+    
+    func testAddEmoji_whenTextIsEmpty_doesNothing() throws {
+        let emojiCount = document.defaultPalette.count
+        
+        document.addEmoji("", toPalette: document.defaultPalette)
+        
+        XCTAssertEqual(emojiCount, document.defaultPalette.count, "Empty String was wrongly added to palette")
+    }
+    
+    func testAddAndRemoveEmoji_whenTextIsEmoji() throws{
+        let emojiCount = document.defaultPalette.count
+        
+        document.addEmoji("🟢", toPalette: document.defaultPalette)
+        XCTAssertEqual(emojiCount+1, document.defaultPalette.count, "Emoji couldn't be added to palette")
+        
+        document.removeEmojis("🟢", fromPalette: document.defaultPalette)
+        XCTAssertEqual(emojiCount, document.defaultPalette.count, "Emoji couldn't be removed from palette")
+        
+    }
+    
+    func testRemoveEmoji_onNonExistingEmoji_doesNothing(){
+        let emojiCount = document.defaultPalette.count
+        
+        document.removeEmojis("🔴", fromPalette: document.defaultPalette)
+        
+        XCTAssertEqual(emojiCount, document.defaultPalette.count)
+    }
+    
+    
+    //add emoji to document on any position
+    func testAddEmoji_onNewDocument(){
+        let documentEmojiCount = document.emojis.count
+        
+        document.addEmoji("⚽️", at: CGPoint(x: 200, y: 200), size: 2)
+        
+        XCTAssertEqual(documentEmojiCount+1, document.emojis.count, "Emoji couldn't be added to document")
+    }
+    
+    
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
     }
 
     func testExample() throws {
